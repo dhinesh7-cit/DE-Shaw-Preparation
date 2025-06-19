80346e420bdad1c10faa941a6c30fa50f2d0351d
Dhinesh Elangovan <mortimersmithgeez@gmail.com>
2025-06-19 13:57:40 +0530

    Create 48.java

---
+++ Day 1/48.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotateAntiClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each column
+        for (int j = 0; j < n; j++) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                int temp = matrix[top][j];
+                matrix[top][j] = matrix[bottom][j];
+                matrix[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+}
+// 🔄 Rotate Matrix 90° Clockwise:
+// 1. Transpose (swap [i][j] with [j][i])
+// 2. Reverse each row
